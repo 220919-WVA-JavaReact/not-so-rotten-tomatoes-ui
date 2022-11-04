8efab870547206fdac224e7e7218b31b15a40b9d
12cwarden <12cwarden@gmail.com>
2022-11-04 16:15:52 -0400

    adding recipe, search, and recipe card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { User } from './models/user';
 import Login from './components/login/login';
 import NaviBar from './components/nav/nav';
@@ -8,6 +10,7 @@ import Home from './components/home/home';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/dashboard';
+import Recipes from './components/recipes/recipes';
 
 function App() {
 
@@ -22,6 +25,7 @@ function App() {
       <Route path="/login" element={<Login currentUser={authUser} setCurrentUser={setAuthUser} />} />
       <Route path="/register" element={<Register currentUser={authUser} setCurrentUser={setAuthUser}/>} />
       <Route path="/dashboard" element={<Dashboard currentUser={authUser} />} />
+      <Route path="/recipes" element ={<Recipes currentUser={authUser} />} />
       </Routes>
     </BrowserRouter>
   );
